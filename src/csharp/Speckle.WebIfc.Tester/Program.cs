7a4ec5d7b72b1eb96e7c48a1600ebdb1d7e56189
adamhathcock <adam@speckle.systems>
2024-11-15 11:18:14 +0000

    linux build works

--- src/csharp/Speckle.WebIfc.Tester/Program.cs
+++ src/csharp/Speckle.WebIfc.Tester/Program.cs
@@ -6,7 +6,7 @@ Console.WriteLine(Environment.CurrentDirectory);
 Console.WriteLine(WebIfc.GetVersion());
 
 var apiPtr = WebIfc.InitializeApi();
-var modelPtr = WebIfc.LoadModel(apiPtr, "/home/adam/git/engine_web-ifc/examples/ifcbridge-model01.ifc");
+var modelPtr = WebIfc.LoadModel(apiPtr, "/home/adam/git/engine_web-ifc/examples/example.ifc");
 var geoCount = WebIfc.GetNumGeometries(modelPtr);
 Console.WriteLine(geoCount);
 WebIfc.FinalizeApi(apiPtr);
